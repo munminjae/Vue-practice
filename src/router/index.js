ca48cae81e4d464d5b8bebb48ca7d00559825ab0
munminjae <oja5068@naver.com>
2024-02-19 21:36:31 +0900

    route 설정

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+//개발할 화면에 해당하는 vue 파일을 import하고 route 안에 특정 path와 매핑해주면 된다.
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+/*
+  path : url 주소 정의
+  name : route로 연결할 때 지정한 이름(선택사항)
+  component : 파일 연결(route에서 불러와질 컴포넌트를 의미)
+*/
+/*
+첫 번째 등록된 route와 두 번째 등록된 route의 차이 : 첫 번째는 이미 import하고 두 번째는 path에 접근하기 전에는 vue 파일에 대한 import가 일어나지 않는다.
+*/
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
